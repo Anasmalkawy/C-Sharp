7055bf8f103686ef2e3ba6d2dd19d3ccd28d46b1
anasmalkawy <anas.malkawy1999@gmail.com>
2025-01-16 16:48:04 +0300

    a

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,142 +1,4 @@
-﻿
-//-user input(read / write)
-
-//3 -
-
-
-//using System;
-
-//namespace Test
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.Write("Enter Name: ");
-//            string text = Console.ReadLine();
-//            Console.WriteLine("You Name: " + text);
-//        }
-//    }
-//}
-
-
-//1-C# variables 
-
-
-
-//using System;
-
-//namespace InputProgram
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//int age = 25;
-//string name = "Alice";
-
-
-//int age;
-//age = 25; // Initialization happens later
-
-
-//int x;
-//int y;
-//int z;
-
-//int x = 1, y = 2, z = 3;
-//int a, s, d;
-
-
-//int number = 100;          // Integer
-//float rate = 10.5f;       // Floating-point number
-//char code = 'A';      // Character
-//bool isValid = true;      // Boolean value
-//string name = "Steve";    // String
-
-//Console.WriteLine(name);
-
-//        }
-//    }
-//}
-
-
-
-
-
-
-
-
-
-
-
-//array 
-//2-
-
-
-//using System;
-
-//namespace InputProgram
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            int[] intArray;
-
-//            // Declare an array of strings
-//            string[] stringArray;
-
-
-//            ////////////////////
-
-
-//            // Initialize with values
-//            int[] intArray = { 1, 2, 3, 4, 5 };
-
-//            // Alternatively, you can use shorthand
-//            string[] stringArray = { "apple", "banana", "cherry" };
-
-//            ////////////////////
-
-
-//            // Accessing elements in a single-dimensional array
-
-//            int firstElement = intArray[0]; // Gets the first element (1)
-//            string secondElement = stringArray[1]; // Gets the second element ("banana")
-
-//            // Change the first element of intArray
-//            intArray[0] = 10; // Now intArray is {10, 2, 3, 4, 5}
-
-//            Console.WriteLine(secondElement);
-
-
-//            int[] numbers = { 1, 2, 3 };
-//for (int i = 0; i < numbers.Length; i++)
-//{
-//    Console.Write(numbers[i]);
-//}
-
-//Console.WriteLine("  ");
-//int first = numbers[0];
-//Console.WriteLine(first);
-
-
-
-
-//numbers[0] = 5;
-//for (int i = 0; i < numbers.Length; i++)
-//{
-//    Console.Write(numbers[i]);
-//}
-
-//        }
-//    }
-//}
-
-
-
-using System;
+﻿using System;
 
 namespace InputProgram
 {
@@ -144,12 +6,14 @@ namespace InputProgram
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("******************************");
+            Console.WriteLine("enter string");
+
             string name = Console.ReadLine();
            
             Console.WriteLine(name);
 
-            ////
-            ///
+            Console.Write("******************************");
 
             int a = 4;
             string b = "aaaa";
@@ -157,31 +21,54 @@ namespace InputProgram
             double d = 4.4533;
             char j = 'f';
             bool aa = true;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(j);
+            Console.WriteLine(aa);
 
-            /////
-            ///
+
+
+            Console.WriteLine("******************************");
+
 
             string[] car = { "audi", "bmw", "toyota" };
             for (int i = 0; i < car.Length; i++)
             {
                 Console.WriteLine(car[i]);
             }
-            ///////
-            ///
+            Console.WriteLine("******************************");
 
-            string fname=Console.ReadLine();
-            string lname = Console.ReadLine();
-            string age = Console.ReadLine();
+            Console.Write("Enter first Name: ");
+            string xx = Console.ReadLine();
+            Console.Write("Enter last Name: ");
+            string cc = Console.ReadLine();
+            Console.Write("Enter age: ");
+            string vv = Console.ReadLine();
 
-            Console.Write(fname, lname, age);
-            /////
-            ///
-            int[] random = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.Write(xx) ;
+            Console.Write(cc) ;
+            Console.Write(vv) ;
+
+
+            Console.WriteLine("******************************");
+
+            string[] random = new string[10];
+            for (int i = 0; i < random.Length; i++)
+            {
+                Console.Write("put to array");
+                string type =Console.ReadLine();
+                random[i] = type;
+            }
             foreach (var i in random)
             {
                 Console.Write(i);
             }
-            /////////
+
+
+            Console.WriteLine("******************************");
+
 
 
 
