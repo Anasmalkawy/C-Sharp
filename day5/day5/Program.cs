3633aabdda9a6f77c015c3c07d27de94460c2ae4
anasmalkawy <anas.malkawy1999@gmail.com>
2025-01-22 16:59:42 +0300

    task4 s

---
+++ day5/day5/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prepare
+{
+    internal class Program
+    {
+       sealed public class carName 
+        {
+           protected string name2;
+        }
+
+        public class carcolor : carName
+        {
+            protected string name = "toyota";
+             public void Vehicle()
+            {
+                Console.WriteLine(name2= "BMW");
+            }
+
+        }
+
+
+
+        public class modelcar:carName
+        {
+            public int model = 1999;
+        }
+
+
+        static void Main(string[] args)
+        {
+            carcolor car = new carcolor();
+            car.Vehicle();
+          
+
+        }
+    }
+}
---
+++ task5/task5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    internal class Program
+    {
+         public class Vechils
+        {
+            public string Brand { get; set; }
+            public int model { get; set; }
+
+            public void start()
+            {
+                Console.WriteLine($"Vehicle is starting.{Brand}{model}");
+            }
+        }
+        public class Derived : Vechils
+        {
+
+            public int NumberOfDoors { get; set; }
+
+
+
+        }
+            static void Main(string[] args)
+        {
+            Derived s = new Derived();
+            s.model = 1;
+            s.Brand = "audi";
+            s.start();
+        }
+    }
+}
+
+
+//What is constructor ? when i want to give the property value 
+
+//What are the basic concepts of OOPs ? 1. Encapsulation 2. Inheritance 3.    Polymorphism 4.    Abstraction
+
+//What is the Encapsulation ? get the privet value and edit it and shaer it 
+
+//What is the sealed class ? class cant shaer thier informtion
+
+//What is the Inheritance concept ? share th informain in classes
